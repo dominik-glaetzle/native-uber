6dd1ed8e185ffac83b6b7171f75512ede54c2d9a
Dominik Glätzle <csba9055@student.uibk.ac.at>
2024-11-02 13:25:56 +0100

    GoogleButton finsihed

---
+++ app/(root)/find-ride.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const find_ride = () => {
+  return (
+    <View>
+      <Text>find-ride</Text>
+    </View>
+  )
+}
+
+export default find_ride
